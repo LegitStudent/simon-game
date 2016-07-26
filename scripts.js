76a546656f807a48e5f70c3e99838a6a451ee632
Jasper Francisco <jasper.francisco@gmail.com>
2016-07-26 21:21:26 +0800

    feedback on strict wrong

--- scripts.js
+++ scripts.js
@@ -36,7 +36,6 @@ var game = {
       var state = this;
       setTimeout(function() {
         state.advanceLevel();
-        state.update();
       }, 1000);
     }
 
@@ -44,13 +43,6 @@ var game = {
   },
   chooseRandomColor: function() {
     return this.colors[Math.floor(Math.random() * 4)];
-  },
-  update: function() {
-    // update view to reflect changes in game object
-    $('#level').text(this.level);
-    $('#checkString').text(this.currentPattern);
-    $('#mainString').text(this.userPattern);
-    $('#checkLevel').text(this.checkLevel);
   },
   userChoice: function(event) {
     this.checkLevel++;
@@ -62,7 +54,15 @@ var game = {
   userMistake: function() {
     this.checkLevel = 0;
     this.userPattern = [];
-    alert('You made a mistake');
+    // alert('You made a mistake');
+    if ($("#strict").prop("checked")) {
+      for (var i = 0; i < this.colors.length; i++) {
+        console.log('playing ' + this.colors[i]);
+        $('.play[data-color="' + this.colors[i] + '"').addClass("blink-" + this.colors[i]);
+        document.getElementById('audio-' + this.colors[i]).play();
+      }
+      this.initialize(true);
+    }
     this.blinkNext(0);
   },
   blinkNext: function(i) {
@@ -75,16 +75,16 @@ var game = {
     var animClass = "";
     switch(this.currentPattern[i]) {
       case 'r':
-        animClass = "blink-red";
+        animClass = "blink-r";
         break;
       case 'b':
-        animClass = "blink-blue";
+        animClass = "blink-b";
         break;
       case 'g':
-        animClass = "blink-green";
+        animClass = "blink-g";
         break;
       case 'y':
-        animClass = "blink-yellow";
+        animClass = "blink-y";
         break;
       default:
         break;
@@ -94,11 +94,15 @@ var game = {
     document.getElementById('audio-' + this.currentPattern[i]).play();
     setTimeout(this.blinkNext.bind(this), 750, i+1);
   },
-  initialize: function() {
+  initialize: function(strict) {
+    // strict is true when user makes a mistake when strict mode is on.
     this.level = 0;
     this.currentPattern = [];
-    this.advanceLevel();
-    this.update();
+    if (strict) {
+      setTimeout(this.advanceLevel.bind(this), 1000);
+    } else {
+      this.advanceLevel();
+    }
   }
 }
 
@@ -108,7 +112,6 @@ function setActive(e) {
   if ($(this).hasClass('no-touch') || game.checkLevel === game.level) return false;
   $(this).addClass('active');
   game.userChoice(e);
-  game.update();
 }
 
 function setInactive(e) {
